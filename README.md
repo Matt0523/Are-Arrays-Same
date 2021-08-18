4f4368eb5892704bd205c5bbcf62bce6e666c380
Matthew McCutcheon <35155706+Matt0523@users.noreply.github.com>
2021-08-17 22:31:52 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+ Are There Arrays The Same?
+
+ Takes two arrays and compares the numbers in order
+ If all the numbers are the same, returns true
+ If the arrays are different, returns false
+
+ To compare the arays
+
+ areArraysSame(a,b)
+
+ EXP:
+
+areArraysSame([1, 2, 3, 4, 5], [1, 2, 3, 4, 8]);
+// false
+
+areArraysSame([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
+// true
---
+++ areArraysTheSame.js
@@ -0,0 +1,19 @@
+/**
+ * * Are There Arrays The Same?
+ * Takes two arrays and compares the numbers in order
+ * If all the numbers are the same, returns true
+ * If the arrays are differant, returns false
+ */
+
+ function areArraysSame(a, b) {
+    var same = true;
+  
+    for (var i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        same = false;
+        return same;
+      }
+    }
+    console.log(same);
+  }
+
